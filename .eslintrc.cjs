fda8056064782fd187774fb5a9f9f23e212ec2bd
phanvantan <pvtan103@gmail.com>
2023-04-25 21:53:21 +0700

    feat: remove unused

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
@@ -11,5 +11,20 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  'import/no-anonymous-default-export': 'off',
+  'no-param-reassign': 'warn',
+  'no-console': [
+    'warn',
+    {
+      allow: ['warn', 'error']
+    }
+  ],
+  'import/order': [
+    'warn',
+    {
+      groups: ['external', 'internal', 'parent', 'sibling', 'index'],
+      'newlines-between': 'always'
+    }
+  ]
 }
